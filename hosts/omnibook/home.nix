afce1d77acaf0fa290ffeebccc3570be238e29ec
adenskyrp <36445815+adenskyrp@users.noreply.github.com>
2026-08-06 12:19:04 -0400

    Established secure SSH remote and finalized Nix architecture

--- hosts/omnibook/home.nix
+++ hosts/omnibook/home.nix
@@ -153,6 +153,12 @@
     };
   };
 
+  programs.git = {
+    enable = true;
+    userName = "adenskyrp";
+    userEmail = "36445815+adenskyrp@users.noreply.github.com";
+  };
+
   # ---------------------------------------------------------------------------
   # INTERACTIVE SHELL ENVIRONMENT (Fish)
   # ---------------------------------------------------------------------------
