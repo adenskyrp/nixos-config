91a36d6e4e26a855c9403ee6e5aa0524d9e8cb97
adenskyrp <36445815+adenskyrp@users.noreply.github.com>
2026-08-06 12:09:50 -0400

    please work

--- modules/core.nix
+++ modules/core.nix
@@ -45,9 +45,11 @@
     # The modern, strictly-typed systemd-resolved configuration
     settings = {
       Resolve = {
+	DNS = "9.9.9.9#dns.quad9.net 1.1.1.1#cloudflare-dns.com";
+        FallbackDNS = "149.112.112.112#dns.quad9.net 1.0.0.1#cloudflare-dns.com";
         DNSSEC = "true";
         Domains = "~.";
-        FallbackDNS = "9.9.9.9#dns.quad9.net 1.1.1.1#cloudflare-dns.com";
+
         
         # STRICT mode DoT configuration
         DNSOverTLS = "yes";
