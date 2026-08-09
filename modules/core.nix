c45bdf6e7d5ccaa54a7cd4d85489ffe6b6856a5e
adenskyrp <36445815+adenskyrp@users.noreply.github.com>
2026-08-09 09:15:51 -0400

    misc. updates

--- modules/core.nix
+++ modules/core.nix
@@ -238,5 +238,6 @@
   environment.pathsToLink = [ "/share/xdg-desktop-portal" "/share/applications" ];
   environment.systemPackages = with pkgs; [
     vim wget ethtool git iw bluetui wireplumber pulsemixer pciutils lm_sensors htop kitty pavucontrol shared-mime-info
+    cargo rustc gcc pkg-config
   ];
 }
