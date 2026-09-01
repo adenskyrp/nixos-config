b611927109a0fa26cfb1ae7ec9c3625e9db94711
adenskyrp <36445815+adenskyrp@users.noreply.github.com>
2026-09-01 08:03:34 -0400

    misc. updates

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -89,6 +89,7 @@
   # ---------------------------------------------------------------------------
   environment.systemPackages = with pkgs; [
     heroic
+    protonup-qt
     mangohud # Frame-time and latency analysis overlay
     usbutils
     evtest
